71fd3ef6df8bdad27f8187734eb23d614cf73928
Wiktor <mcserw4@wp.pl>
2023-12-02 22:45:24 +0100

    day 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,48 +1,46 @@
+use regex::Regex;
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
+    let mut bag: HashMap<&str, i32> = HashMap::from([("red",12), ("green", 13), ("blue", 14)]);
+
     let contents = fs::read_to_string("data.txt").expect("Error");
     let games = contents.split("\n").map(|it| it.trim()).collect::<Vec<&str>>();
 
-    let red_cubes = 12;
-    let green_cubes = 13;
-    let blue_cubes = 14;
+    let mut result: i32 = 0;
 
-    let mut counter = 0;
     for game in games {
-        let formatted = game.replace("Game ", "");
-        let data = formatted.split(":").map(|it| it.trim()).collect::<Vec<&str>>();
-        if data.len() != 2 { continue }
-        let game_id = data.first().unwrap().parse::<i32>().unwrap();
-        let sets = data.get(1).unwrap().split(";").collect::<Vec<&str>>();
-        let mut is_possible = true;
-        for set in sets {
-            let items = set.trim().split(", ").collect::<Vec<&str>>();
-            for item in items {
-                let d = item.split(" ").collect::<Vec<&str>>();
-                let amount = d.first().unwrap().parse::<i32>().unwrap();
-                let name = d.last().unwrap();
+        let mut is_possible: bool = true;
+        let mut game_id: i32 = 0;
 
-                match name {
-                    &"red" => {
-                        is_possible = amount < red_cubes;
-                    }
-                    &"green" => {
-                        is_possible = amount < green_cubes;
-                    }
-                    &"blue" => {
-                        is_possible = amount < blue_cubes;
-                    }
-                    _ => {
-                        break
+        if let Some(captures) = Regex::new(r"\d\d?\d?:").unwrap().captures(&game) {
+            if let Some(id) = captures.get(0) {
+                game_id = id.as_str().replace(":", "").parse().unwrap();
+            }
+        }
+        let draws: Vec<&str> = Regex::new(r"\d\d? \w*").unwrap().find_iter(&game).map(|m| m.as_str()).collect();
+
+        for draw in draws {
+            let split: Vec<&str> = draw.split_whitespace().collect();
+            if split.len() == 2 {
+                let num: i32 = split[0].parse().unwrap();
+                let color: &str = split[1];
+                let mut cubes = HashMap::new();
+                cubes.insert(color, num);
+
+                for (cube_color, cube_num) in &cubes {
+                    if let Some(&bag_num) = bag.get(cube_color) {
+                        if *cube_num > bag_num {
+                            is_possible = false;
+                        }
                     }
                 }
             }
-            if !is_possible { break }
         }
         if is_possible {
-            counter += game_id;
+            result += game_id;
         }
     }
-    println!("{}", counter);
+    println!("{}", result);
 }
